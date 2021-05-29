55af988b69aeaf3c44d71b8c56fe50d44276ddf8
howkawgew <73640655+howkawgew@users.noreply.github.com>
2021-05-29 19:09:40 +0300

    абчихба

--- lib/db/db.py
+++ lib/db/db.py
@@ -1,9 +1,10 @@
 import pymysql
+import sys
 
 HOST = '54.37.129.120'
 PORT = 3306
 USER = 'pepega'
-PASSWORD = 'pepega'
+PASSWORD = ''
 DATABASE = 'plasmo_rp'
 debug = False
 
@@ -13,60 +14,75 @@ conn = pymysql.connect(host=HOST,
                        passwd=PASSWORD,
                        db=DATABASE)
 
+
 def select(table='parliament_votes', columns='*', where='', args='', always_return_all=False, return_list=False,
            return_matrix=False):
-  
-    if where != '':
-        where = 'WHERE ' + where
-    request = f'SELECT {columns} FROM {table} {where} {args}'
-    if debug:
-        print(request)
-    cur = conn.cursor()
-    response = cur.execute(request)
-    cur.close()
-    if return_list:
-        response = []
-        for elem in cur.fetchall():
-            response.append(elem[0])
-        return response
-    elif return_matrix:
-        response = []
-        for elem in cur.fetchall():
-
-            response.append([elem[1], int(elem[0])])
-        return response
-    if response <= 1 and not always_return_all:
-        return cur.fetchone()
-    else:
-        return cur.fetchall()
+    try:
+        if where != '':
+            where = 'WHERE ' + where
+        request = f'SELECT {columns} FROM {table} {where} {args}'
+        if debug:
+            print(request)
+        cur = conn.cursor()
+        response = cur.execute(request)
+        cur.close()
+        if return_list:
+            response = []
+            for elem in cur.fetchall():
+                response.append(elem[0])
+            return response
+        elif return_matrix:
+            response = []
+            for elem in cur.fetchall():
+                response.append([elem[1], int(elem[0])])
+            return response
+        if response <= 1 and not always_return_all:
+            return cur.fetchone()
+        else:
+            return cur.fetchall()
+    except Exception as err:
+        print(err.__class__, err)
+        sys.exit(1)
 
 
 def insert(data, table='parliament_votes'):
-    request = f'INSERT INTO {table} SET {data}'
-    if debug:
-        print(request)
-    cur = conn.cursor()
-    cur.execute(request)
-    conn.commit()
-    cur.close()
-    return True
+    try:
+        request = f'INSERT INTO {table} SET {data}'
+        if debug:
+            print(request)
+        cur = conn.cursor()
+        cur.execute(request)
+        conn.commit()
+        cur.close()
+        return True
+    except Exception as err:
+        print(err.__class__, err)
+        sys.exit(1)
 
 
 def delete(where, table='parliament_votes'):
-    request = f'DELETE FROM {str(table)} WHERE {str(where)}'
-    if debug:
-        print(request)
-    cur = conn.cursor()
-    cur.execute(request)
-    conn.commit()
-    cur.close()
+    try:
+        request = f'DELETE FROM {str(table)} WHERE {str(where)}'
+        if debug:
+            print(request)
+        cur = conn.cursor()
+        cur.execute(request)
+        conn.commit()
+        cur.close()
+    except Exception as err:
+        print(err.__class__, err)
+        sys.exit(1)
 
 
 def update(data, where, table='parliament_votes'):
-    request = f'UPDATE {table} SET {data} WHERE {where}'
-    if debug:
-        print(request)
-    cur = conn.cursor()
-    cur.execute(request)
-    conn.commit()
-    cur.close()
+    try:
+        request = f'UPDATE {table} SET {data} WHERE {where}'
+        if debug:
+            print(request)
+        cur = conn.cursor()
+        cur.execute(request)
+        conn.commit()
+        cur.close()
+    except Exception as err:
+        print(err.__class__, err)
+        sys.exit(1)
