de3679aa29c4aecaf889a515fafcac56f8693811
howkawgew <zarbazan4ik@ya.ru>
2022-05-16 23:21:53 +0300

    feat: setup, set_user_vote, get_user_vote, get_candidate_votes, get_candidates, reset_candidate_votes

--- mkovotebot/utils/database.py
+++ mkovotebot/utils/database.py
@@ -1,13 +1,17 @@
 from __future__ import annotations
 
+import asyncio
 from dataclasses import dataclass
 from typing import List
+import logging
 
 import aiosqlite as aiosqlite
 
 from mkovotebot import settings
 
+logger = logging.getLogger(__name__)
 PATH = settings.DATABASE_PATH
+
 
 
 @dataclass
@@ -16,41 +20,48 @@ class Candidate:
     votes_count: int
 
 
+# I know that using f-strings in SQL queries is not allowed, but I did not find another way
+# Also self._table is a checked value, so the chance of SQL injection is 0%.
+
+
 class MKODatabase:
     def __init__(self, table: str):
         if table not in ["mko_votes", "president_votes"]:
             raise ValueError(f"{table} table is not allowed")
-        self.table = table
+        self._table = table
 
     async def setup(self):
         """
         Create database if not exist
-        :return:
         """
-        query = """
-        CREATE TABLE IF NOT EXISTS ?
+        logger.debug("[%s] Setting up database", self._table)
+
+        query = f"""
+        CREATE TABLE IF NOT EXISTS {self._table}
         (
             voter_id     integer not null,
-            candidate_id integer not null
+            candidate_id integer not null,
+            unique (voter_id)
         );
-
-        create unique index president_votes_voter_id_uindex
-            on ? (voter_id);
         """
         async with aiosqlite.connect(PATH) as db:
-            await db.execute(query, (self.table, self.table))
+            await db.execute(query)
             await db.commit()
 
     async def set_user_vote(
         self, voter_plasmo_id: int, candidate_plasmo_id: int | None
     ):
+        logger.debug(
+            "[%s] reset_candidate_votes is called with %s",
+            self._table,
+            candidate_plasmo_id,
+        )
         async with aiosqlite.connect(PATH) as db:
             if candidate_plasmo_id is not None:
                 await db.execute(
-                    """INSERT INTO ?(voter_id, candidate_id) VALUES(?, ?)
+                    f"""INSERT INTO {self._table}(voter_id, candidate_id) VALUES(?, ?)
            ON CONFLICT(voter_id) DO UPDATE SET candidate_id=?""",
                     (
-                        self.table,
                         voter_plasmo_id,
                         candidate_plasmo_id,
                         candidate_plasmo_id,
@@ -58,34 +69,69 @@ class MKODatabase:
                 )
             else:
                 await db.execute(
-                    """DELETE FROM ? WHERE voter_id = ?""",
-                    (self.table, voter_plasmo_id),
+                    f"""DELETE FROM {self._table} WHERE voter_id = ?""",
+                    (voter_plasmo_id,),
                 )
 
             await db.commit()
-        return True
 
     async def reset_candidate_votes(self, candidate_plasmo_id: int) -> List[int]:
+        logger.debug(
+            "[%s] reset_candidate_votes is called with %s",
+            self._table,
+            candidate_plasmo_id,
+        )
         candidate_votes = await self.get_candidate_votes(candidate_plasmo_id)
         async with aiosqlite.connect(PATH) as db:
             await db.execute(
-                """DELETE FROM ? WHERE candidate_id = ?""",
-                (self.table, candidate_plasmo_id),
+                f"""DELETE FROM {self._table} WHERE candidate_id = ?""",
+                (candidate_plasmo_id,),
             )
 
             await db.commit()
         return candidate_votes
 
-    async def get_user_vote(self, voter_plasmo_id: int) -> int:
-        ...  # TODO
+    async def get_user_vote(self, voter_plasmo_id: int) -> int | None:
+        logger.debug(
+            "[%s] get_user_vote is called with %s", self._table, voter_plasmo_id
+        )
+        async with aiosqlite.connect(PATH) as db:
+            async with db.execute(
+                f"""SELECT candidate_id FROM {self._table} WHERE voter_id = ?""",
+                (voter_plasmo_id,),
+            ) as cursor:
+                if vote := await cursor.fetchone():
+                    return vote[0]
+                else:
+                    return None
 
     async def get_candidate_votes(self, candidate_plasmo_id: int) -> List[int]:
-        ...  # TODO
-        return [4]
+        logger.debug(
+            "[%s] get_candidate_votes is called with %s",
+            self._table,
+            candidate_plasmo_id,
+        )
+        async with aiosqlite.connect(PATH) as db:
+            async with db.execute(
+                f"""SELECT voter_id FROM {self._table} WHERE candidate_id = ?""",
+                (candidate_plasmo_id,),
+            ) as cursor:
+                return [voter[0] for voter in await cursor.fetchall()]
 
-    async def get_candidates(self, sort_descending=False) -> List[Candidate]:
-        ...  # TODO
-        return [Candidate(plasmo_id=433, votes_count=5)]
+    async def get_candidates(self) -> List[Candidate]:
+        logger.debug(
+            "[%s] get_candidates is called",
+            self._table,
+        )
+
+        async with aiosqlite.connect(PATH) as db:
+            async with db.execute(
+                f"""SELECT candidate_id, COUNT(*) FROM {self._table} GROUP BY candidate_id""",
+            ) as cursor:
+                return [
+                    Candidate(plasmo_id=raw_candidate[0], votes_count=raw_candidate[1])
+                    for raw_candidate in await cursor.fetchall()
+                ]
 
 
 class PresidentElectionsDatabase(MKODatabase):
