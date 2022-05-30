4b25537abbc6ab1701e13c25152c883caca4588d
howkawgew <zarbazan4ik@ya.ru>
2022-05-30 18:32:45 +0300

    Removed user voting

--- mkovotebot/__main__.py
+++ mkovotebot/__main__.py
@@ -20,9 +20,11 @@ if settings.Config.mko_voting_enabled:
     asyncio.run(database.setup())
     del database
 
+    """
     if settings.Config.user_voting_enabled:
         logger.debug("Loading mkovotebot.ext.mko_user_voting")
         bot.load_extension("mkovotebot.ext.mko_user_voting")
+    """
 
 if settings.Config.president_voting_enabled:
     logger.debug("Loading mkovotebot.ext.president_voting")
